df42586b0f4141bf970e8056152642b9131bc9fb
Romain Carnus <romain.carnus@gmail.com>
2020-04-08 11:41:14 -0400

    download.py: Add requests and wget libraries options

--- download.py
+++ download.py
@@ -1,5 +1,9 @@
-import urllib2
 import sys
+
+
+urllib2import = True
+wgetimport = True
+requestsimport = True
 
 def usage():
     print("download.py [URL] [FILENAME]")
@@ -12,8 +16,52 @@ if len(sys.argv) < 3:
 url = sys.argv[1]
 filename = sys.argv[2]
 
-filedata = urllib2.urlopen(url)
-datatowrite = filedata.read()
+try:
+    import urllib2
+except ImportError:
+    print("urllib2 not found")
+    urllib2import = False
 
-with open(filename,'wb') as fd:
-    fd.write(datatowrite)
+try:
+    import requests
+except ImportError:
+    print("requests not found")
+    requestsimport = False
+
+try:
+    import wget
+except ImportError:
+    print("wget not found")
+    wgetimport = False
+
+if urllib2import == True:
+    print("Downloading with urllib2")
+    #With urllib2
+    filedata = urllib2.urlopen(url)
+    datatowrite = filedata.read()
+    with open(filename,'wb') as fd:
+        fd.write(datatowrite)
+    print("Done")
+    sys.exit(0)
+
+if requestsimport == True:
+    print("Downloading with requests")
+    #With requests
+    myfile = requests.get(url)
+    datatowrite = myfile.content
+    with open(filename,'wb') as fd:
+        fd.write(datatowrite)
+    print("Done")
+    sys.exit(0)
+
+if wgetimport == True:
+    print("Downloading with wget")
+    #With wget
+    wget.download(url, filename)
+    print("Done")
+    sys.exit(0)
+
+
+print("No suitable module found. I suggest to install one of them")
+sys.exit(1)
+
