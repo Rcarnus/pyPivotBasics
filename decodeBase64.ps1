a33302905a38886f2db9535ff7732c7b0189fadc
Romain Carnus <romain.carnus@gmail.com>
2024-12-20 15:51:15 -0500

    encodeBase64: Add simple Powershell encode and decode scripts.

---
+++ decodeBase64.ps1
@@ -0,0 +1,8 @@
+﻿$InputFileName = "C:\Users\Rom\Documents\newcer\orig\newcer.exe.b64.zip.b64"
+$OutpuFileName = "C:\Users\Rom\Documents\newcer\newcer.exe.b64.zip"
+
+$base64string = ""
+$Base64FileName = "C:\Users\Rom\Documents\newcer\orig\newcer.exe.b64"
+$base64string = [Convert]::FromBase64String([System.Text.Encoding]::UTF8.GetString([IO.File]::ReadAllBytes($InputFileName)))
+[IO.File]::WriteAllBytes($OutpuFileName, [system.Text.Encoding]::UTF8.GetBytes($base64string))
+Get-FileHash $OutpuFileName
---
+++ encodeBase64.ps1
@@ -0,0 +1,8 @@
+﻿$InputFileName = "C:\Users\Rom\Documents\newcer\orig\newcer.exe.b64.zip"
+$OutpuFileName = "C:\Users\Rom\Documents\newcer\orig\newcer.exe.b64.zip.b64"
+
+$base64string = ""
+$FileName = $InputFileName
+$base64string = [Convert]::ToBase64String([IO.File]::ReadAllBytes($InputFileName))
+[IO.File]::WriteAllBytes($OutpuFileName, [system.Text.Encoding]::UTF8.GetBytes($base64string))
+Get-FileHash $OutpuFileName
